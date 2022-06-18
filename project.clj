1ed3ab47492c5a0ca19dce9753937804146d20c0
MelKori <mieldo@gmail.com>
2022-06-19 01:35:13 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ai.z7/java-properties "1.0.0"
+  :description "Java properties files micro parcer for Clojure"
+  :url "https://github.com/ai.z7/java-properties"
+  :license {:name "MIT"
+            :url  "https://github.com/fern-flower-lab/java-properties/LICENSE"}
+  :dependencies [])
---
+++ src/java_properties/core.clj
@@ -0,0 +1,2 @@
+(ns java-properties.core)
+
