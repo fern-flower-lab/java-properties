a623537acc11f05e5b50ac30844d044c8a01b933
MelKori <mieldo@gmail.com>
2022-06-19 15:41:10 +0300

    update descr

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject ai.z7/java-properties "1.0.0"
-  :description "Java properties files micro parcer for Clojure"
+  :description "Java properties files micro parser for Clojure"
   :url "https://github.com/fern-flower-lab/java-properties"
   :license {:name "MIT"
-            :url  "https://github.com/fern-flower-lab/java-properties/LICENSE"}
-  :dependencies [])
+            :url  "https://github.com/fern-flower-lab/java-properties/LICENSE"})
