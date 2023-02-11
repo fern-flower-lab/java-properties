328ca3af6b877d0f71048265eb5ea310d1fdb995
MelKori <mieldo@gmail.com>
2023-02-11 15:11:05 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ai.z7/java-properties "1.1.3"
+(defproject ai.z7/java-properties "1.2.0"
   :description "Java properties files micro parser for Clojure"
   :url "https://github.com/fern-flower-lab/java-properties"
   :license {:name "MIT"
