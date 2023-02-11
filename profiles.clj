26e50a49711dff66a76f65e9d5ff39124f869e37
MelKori <mieldo@gmail.com>
2023-02-11 15:03:23 +0200

    upgrade development profile dependencies

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {:dev      {:global-vars  {*warn-on-reflection* true}
             :dependencies [[org.clojure/clojure "1.11.1"]
-                           [ch.qos.logback/logback-classic "1.2.11"
+                           [ch.qos.logback/logback-classic "1.4.5"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [org.slf4j/jul-to-slf4j "1.7.36"]
-                           [org.slf4j/jcl-over-slf4j "1.7.36"]
-                           [org.slf4j/log4j-over-slf4j "1.7.36"]
+                           [org.slf4j/jul-to-slf4j "2.0.6"]
+                           [org.slf4j/jcl-over-slf4j "2.0.6"]
+                           [org.slf4j/log4j-over-slf4j "2.0.6"]
                            [org.clojure/tools.logging "1.2.4"]]}
  :provided {:jar-exclusions [#"test.properties"]}}
