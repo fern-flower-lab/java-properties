45cd0faa244bba588fc93f8953f36f28b568722b
A I <io-devel@copper.mx>
2025-08-15 00:08:42 +0300

    update dependecies

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {:dev      {:global-vars  {*warn-on-reflection* true}
-            :dependencies [[org.clojure/clojure "1.11.1"]
-                           [ch.qos.logback/logback-classic "1.4.5"
+            :dependencies [[org.clojure/clojure "1.12.1"]
+                           [ch.qos.logback/logback-classic "1.5.18"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [org.slf4j/jul-to-slf4j "2.0.6"]
-                           [org.slf4j/jcl-over-slf4j "2.0.6"]
-                           [org.slf4j/log4j-over-slf4j "2.0.6"]
-                           [org.clojure/tools.logging "1.2.4"]]}
+                           [org.slf4j/jul-to-slf4j "2.0.17"]
+                           [org.slf4j/jcl-over-slf4j "2.0.17"]
+                           [org.slf4j/log4j-over-slf4j "2.0.17"]
+                           [org.clojure/tools.logging "1.3.0"]]}
  :provided {:jar-exclusions [#"test.properties"]}}
